d3bc57b06171a25d11839754904c629a6dadaf45
Salman Ahmad <salmanahmad111499@gmail.com>
2024-05-07 07:53:59 +0500

    added castling

--- Chess Game Console Based/chess.cpp
+++ Chess Game Console Based/chess.cpp
@@ -2,11 +2,14 @@
  */
 
 #include "chess.h"
+#include <Windows.h>
 
 int main(int argc, char* argv[]) {
 
     // declare a new chess game/board
     Board b; // creates the squares, puts white and black pieces
+    Move m;
+    auto legal_move_flag = false;
 
     // while game not done
     bool quit = false;
@@ -15,7 +18,7 @@ int main(int argc, char* argv[]) {
         cout << b << endl;
 
         // get their move
-        Move m;
+        //Move m;
         do {
             // h6 - let them know to type q1 q2 to quit
             cout << "(To quit, type q1 q2 and enter or return.)" << endl;
@@ -34,10 +37,11 @@ int main(int argc, char* argv[]) {
             }
 
             // check that it is a legal move
-            if (!m.is_legal(b)) {
+            legal_move_flag = !m.is_legal(b);
+            if (legal_move_flag) {
                 cout << "Not legal. Try again." << endl;
             }
-        } while (!m.is_legal(b));
+        } while (legal_move_flag);
 
         // h6 - check flag whether they want to quit
         if (quit) break;
@@ -149,16 +153,143 @@ void Board::do_move(Move* m) {
     int from_i = which_index(m->from.file, m->from.rank);
     int to_i = which_index(m->to.file, m->to.rank);
 
-    // h7 - make sure to delete a piece if being removed
-    if (b[to_i].p != NULL)
-        //delete b[to_i].p;
+    // check if its a pawn
+    if (b[from_i].p->type == 'p')
+    {
+        // check if its a white piece
+        if (b[from_i].p->white)
+        {
+            // yes it is
+            // check its rank of destination
+            if (m->to.rank == 8)
+            {
+                // ask user
+                char what = ' ';
+                cout << "Your pawn is being promoted. Do you want a (q)ueen, (r)ook, (b)ishop, or k(n)ight?" << endl;
+                cin >> what;
+                switch (what)
+                {
+                case 'q':
+                    // delete from and put new piece there
+                    delete b[from_i].p;
+                    b[from_i].p = new Queen(true);
+                    break;
+                case 'r':
+                    // delete from and put new piece there
+                    delete b[from_i].p;
+                    b[from_i].p = new Rook(true);
+                    break;
+                case 'b':
+                    // delete from and put new piece there
+                    delete b[from_i].p;
+                    b[from_i].p = new Bishop(true);
+                    break;
+                case 'n':
+                    // delete from and put new piece there
+                    delete b[from_i].p;
+                    b[from_i].p = new Knight(true);
+                    break;
+                }
+            }
+        }
+        else
+        {
+            // no it isn't
+            // check its rank of destination
+            // ask user
+            char what = ' ';
+            cout << "Your pawn is being promoted. Do you want a (q)ueen, (r)ook, (b)ishop, or k(n)ight?" << endl;
+            cin >> what;
+            switch (what)
+            {
+            case 'q':
+                // delete from and put new piece there
+                delete b[from_i].p;
+                b[from_i].p = new Queen(true);
+                break;
+            case 'r':
+                // delete from and put new piece there
+                delete b[from_i].p;
+                b[from_i].p = new Rook(true);
+                break;
+            case 'b':
+                // delete from and put new piece there
+                delete b[from_i].p;
+                b[from_i].p = new Bishop(true);
+                break;
+            case 'n':
+                // delete from and put new piece there
+                delete b[from_i].p;
+                b[from_i].p = new Knight(true);
+                break;
+            }
+        }
+    }
 
-    // put piece from from_i to to_i
-    b[to_i].p = b[from_i].p;
+    // check for castling corrdintes
+    bool castling_flag = false;
+    if (m->from.file == 'e' && m->from.rank == 1)
+    {
+        castling_flag = true;
+        // for white king
+        // check for to coordintates
+        if (m->to.file == 'g' && m->to.rank == 1)
+        {
+            // moving right
+            b[63].p = NULL;
+            b[61].p = new Rook(true);
+            delete b[from_i].p;
+            b[from_i].p = NULL;
+            b[to_i].p = new King(true);
+        }
+        else if (m->to.file == 'c' && m->to.rank == 1)
+        {
+            // moving left
+            b[56].p = NULL;
+            b[57].p = new Rook(true);
+            delete b[from_i].p;
+            b[from_i].p = NULL;
+            b[to_i].p = new King(true);
+        }
+    }
+    else if (m->from.file == 'e' && m->from.rank == 8)
+    {
+        castling_flag = true;
+        // for black king
+        // check for to coordintates
+        if (m->to.file == 'g' && m->to.rank == 8)
+        {
+            // moving right
+            b[7].p = NULL;
+            b[5].p = new Rook(false);
+            delete b[from_i].p;
+            b[from_i].p = NULL;
+            b[to_i].p = new King(false);
+        }
+        else if (m->to.file == 'c' && m->to.rank == 8)
+        {
+            // moving left
+            b[0].p = NULL;
+            b[1].p = new Rook(false);
+            delete b[from_i].p;
+            b[from_i].p = NULL;
+            b[to_i].p = new King(false);
+        }
+    }
 
-    b[from_i].p = NULL;
+    if (!castling_flag)
+    {
+        // h7 - make sure to delete a piece if being removed
+        if (b[to_i].p != NULL)
+            delete b[to_i].p;
 
-    white_to_move = !(white_to_move);
+        // put piece from from_i to to_i
+        b[to_i].p = b[from_i].p;
+
+        b[from_i].p = NULL;
+
+        white_to_move = !(white_to_move);
+    }
 }
 
 
@@ -184,13 +315,45 @@ istream& operator>>(istream& is, Position& p) {
 
 bool Move::is_legal(const Board b) {
     // things to check...
+    int from_i = b.which_index(from.file, from.rank);
+    int to_i = b.which_index(to.file, to.rank);
+
+    // check for castling coordintes here
+    if (from.file == 'e' && from.rank == 1)
+    {
+        // for white king
+        // check for to coordintates
+        if (to.file == 'g' && to.rank == 1)
+        {
+            b.b[from_i].p->legal_move(&b, this);
+            return true;
+        }
+        else if (to.file == 'c' && to.rank == 1)
+        {
+            b.b[from_i].p->legal_move(&b, this);
+            return true;
+        }
+    }
+    else if (from.file == 'e' && from.rank == 8)
+    {
+        // for black king
+        // check for to coordintates
+        if (to.file == 'g' && to.rank == 8)
+        {
+            b.b[from_i].p->legal_move(&b, this);
+            return true;
+        }
+        else if (to.file == 'c' && to.rank == 8)
+        {
+            b.b[from_i].p->legal_move(&b, this);
+            return true;
+        }
+    }
 
     // check that there is a piece on that square
-    int from_i = b.which_index(from.file, from.rank);
     if (b.b[from_i].p == NULL) return false;
 
     // check that we're moving the right player's piece
-    int to_i = b.which_index(to.file, to.rank);
     if (b.white_to_move != b.b[from_i].p->white) {
         return false;
     }
@@ -205,6 +368,25 @@ bool Move::is_legal(const Board b) {
     // given the piece being moved and the board, is it legal
     if (!b.b[from_i].p->legal_move(&b, this))
         return false;
+
+    // checks for pawn
+    if (b.b[from_i].p->type == 'p')
+    {
+        // if its a pawn
+        // check for its rank difference
+        if (to_i > from_i)
+        {
+            // if pawn is moving downwards
+            if ((to_i - from_i) > 16)
+                return false;
+        }
+        else if (from_i > to_i)
+        {
+            // pawn is moving topwards
+            if ((from_i - to_i) > 16)
+                return false;
+        }
+    }
 
     return true;
 }
@@ -323,7 +505,7 @@ bool Bishop::_do_move(const Board* b, const Move* m)
                     {
                         // if pieces are not the same and this is the end location of the Rook
                         // remove piece and return
-                        delete b->b[i].p;
+                        //delete b->b[i].p;
                         return true;
                     }
                     if ((b->b[i].p->white != b->b[index].p->white) && i != end)
@@ -363,7 +545,7 @@ bool Bishop::_do_move(const Board* b, const Move* m)
                     {
                         // if pieces are not the same and this is the end location of the Rook
                         // remove piece and return
-                        delete b->b[i].p;
+                        //delete b->b[i].p;
                         return true;
                     }
                     if ((b->b[i].p->white != b->b[index].p->white) && i != end)
@@ -407,7 +589,7 @@ bool Bishop::_do_move(const Board* b, const Move* m)
                     {
                         // if pieces are not the same and this is the end location of the Rook
                         // remove piece and return
-                        delete b->b[i].p;
+                        //delete b->b[i].p;
                         return true;
                     }
                     if ((b->b[i].p->white != b->b[index].p->white) && i != end)
@@ -447,7 +629,7 @@ bool Bishop::_do_move(const Board* b, const Move* m)
                     {
                         // if pieces are not the same and this is the end location of the Rook
                         // remove piece and return
-                        delete b->b[i].p;
+                        //delete b->b[i].p;
                         return true;
                     }
                     if ((b->b[i].p->white != b->b[index].p->white) && i != end)
@@ -574,7 +756,7 @@ bool Rook::_do_move(const Board* b, const Move* m)
                     {
                         // if pieces are not the same and this is the end location of the Rook
                         // remove piece and return
-                        delete b->b[i].p;
+                        //delete b->b[i].p;
                         return true;
                     }
                     if ((b->b[i].p->white != b->b[index].p->white) && i != end)
@@ -614,7 +796,7 @@ bool Rook::_do_move(const Board* b, const Move* m)
                     {
                         // if pieces are not the same and this is the end location of the Rook
                         // remove piece and return
-                        delete b->b[i].p;
+                        //delete b->b[i].p;
                         return true;
                     }
                     if ((b->b[i].p->white != b->b[index].p->white) && i != end)
@@ -786,13 +968,184 @@ bool King::legal_move(const Board* b, const Move* m) {
     // call the generic function, and make sure it passes
     if (!Piece::legal_move(b, m)) return false;
 
-    // checking how the king moves
-    if (abs(m->from.file - m->to.file) > 1 ||
-        abs(m->from.rank - m->to.rank) > 1) return false;
+    //// checking how the king moves
+    //if (abs(m->from.file - m->to.file) > 1 ||
+    //    abs(m->from.rank - m->to.rank) > 1) return false;
+
+    // my code from here
+    int r = 0;
+    switch (m->from.rank)
+    {
+    case 1:
+        r = 8 * 7;
+        break;
+    case 2:
+        r = 8 * 6;
+        break;
+    case 3:
+        r = 8 * 5;
+        break;
+    case 4:
+        r = 8 * 4;
+        break;
+    case 5:
+        r = 8 * 3;
+        break;
+    case 6:
+        r = 8 * 2;
+        break;
+    case 7:
+        r = 8 * 1;
+        break;
+    case 8:
+        r = 8 * 0;
+        break;
+    }
+    int index = (r)+(m->from.file - 96) - (1);
+    switch (m->to.rank)
+    {
+    case 1:
+        r = 8 * 7;
+        break;
+    case 2:
+        r = 8 * 6;
+        break;
+    case 3:
+        r = 8 * 5;
+        break;
+    case 4:
+        r = 8 * 4;
+        break;
+    case 5:
+        r = 8 * 3;
+        break;
+    case 6:
+        r = 8 * 2;
+        break;
+    case 7:
+        r = 8 * 1;
+        break;
+    case 8:
+        r = 8 * 0;
+        break;
+    }
+    int end = (r)+(m->to.file - 96) - (1);
+    // perform castling
+    // check piece color
+    if (b->b[index].p->white)
+    {
+        // if its a white king
+        // check if white king is on e1
+        if (m->from.file == 'e' && m->from.rank == 1)
+        {
+            if (m->to.file == 'g')
+            {
+                // castling towards right
+                // move night to the side of king (left) one square
+                if (b->b[61].p)
+                {
+                    // if a piece is already present there
+                    // delete rook from its place and the piece also
+                    // move rook to the side of the king
+                    // deleteing rook
+                    delete b->b[63].p;
+                    // deleting piece
+                    delete b->b[61].p;
+                    return true;
+                }
+                else
+                {
+                    // delete rook only
+                    delete b->b[63].p;
+                    return true;
+                }
+            }
+            else if (m->to.file == 'c')
+            {
+                // castling towards left
+                // move night to the side of king (right) one square
+                if (b->b[58].p)
+                {
+                    // if a piece is already present there
+                    // delete rook from its place and the piece also
+                    // move rook to the side of the king
+                    // deleteing rook
+                    delete b->b[56].p;
+                    // deleting piece
+                    delete b->b[58].p;
+                    return true;
+                }
+                else
+                {
+                    // delete rook only
+                    delete b->b[56].p;
+                    return true;
+                }
+            }
+        }
+        else
+            return false;
+    }
+    else if (b->b[index].p->white == false)
+    {
+        // if its a black king
+        // if its a white king
+        // check if white king is on e1
+        if (m->from.file == 'e' && m->from.rank == 8)
+        {
+            if (m->to.file == 'g')
+            {
+                // castling towards right
+                // move night to the side of king (left) one square
+                if (b->b[5].p)
+                {
+                    // if a piece is already present there
+                    // delete rook from its place and the piece also
+                    // move rook to the side of the king
+                    // deleteing rook
+                    delete b->b[7].p;
+                    // deleting piece
+                    delete b->b[5].p;
+                    return true;
+                }
+                else
+                {
+                    // delete rook only
+                    delete b->b[7].p;
+                    return true;
+                }
+            }
+            else if (m->to.file == 'c')
+            {
+                // castling towards left
+                // move night to the side of king (right) one square
+                if (b->b[2].p)
+                {
+                    // if a piece is already present there
+                    // delete rook from its place and the piece also
+                    // move rook to the side of the king
+                    // deleteing rook
+                    delete b->b[0].p;
+                    // deleting piece
+                    delete b->b[2].p;
+                    return true;
+                }
+                else
+                {
+                    // delete rook only
+                    delete b->b[0].p;
+                    return true;
+                }
+            }
+        }
+        else
+            return false;
+    }
 
     return true;
 }
 
+// done
 bool Queen::legal_move(const Board* b, const Move* m) {
     // call the generic function, and make sure it passes
     if (!Piece::legal_move(b, m)) return false;
